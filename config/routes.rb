60f67140c153bb37fdc3635758b380c15e0dd81d
Maktech07 <maktech07@gmail.com>
2015-06-10 23:00:06 -0500

    Adding of Person data type, full scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :people
   get 'statics/home'
 
   get 'statics/help'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611013713) do
+ActiveRecord::Schema.define(version: 20150611035759) do
+
+  create_table "people", force: :cascade do |t|
+    t.text     "firstName"
+    t.text     "lastName"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
